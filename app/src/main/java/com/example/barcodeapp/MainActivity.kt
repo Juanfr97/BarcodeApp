88c13247afef75a9f8aaa815081107a964f929ca
juanfrausto <juanfr97@hotmail.com>
2024-03-21 19:54:28 -0600

    Barcode implemented

--- app/src/main/java/com/example/barcodeapp/MainActivity.kt
+++ app/src/main/java/com/example/barcodeapp/MainActivity.kt
@@ -2,10 +2,59 @@ package com.example.barcodeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.barcodeapp.adapters.ProductAdapter
+import com.example.barcodeapp.models.Product
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button : Button
+    private lateinit var productsRecyclerView: RecyclerView
+    private val products = mutableListOf<Product>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.btn_scan)
+        setUpRecyclerView()
+        button.setOnClickListener {
+            scanCode()
+        }
+    }
+
+    private fun setUpRecyclerView(){
+        productsRecyclerView = findViewById(R.id.rv_products)
+        productsRecyclerView.adapter = ProductAdapter(products)
+        productsRecyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+    }
+
+    private fun scanCode(){
+        val options = ScanOptions()
+        options.setPrompt("Escanee un codigo de barras")
+        options.setBeepEnabled(true)
+        options.setOrientationLocked(false)
+        barcodeLauncher.launch(options)
+    }
+
+    private val barcodeLauncher = registerForActivityResult(ScanContract()){ result ->
+        if(result.contents == null){
+            // El usuario cerro la camara sin escanear nada
+            Toast.makeText(this,"Cancelado",Toast.LENGTH_LONG).show()
+        }
+        else{
+            // El usuario capturo un codigo de barras o un QR
+            val product = Product.products.find { p-> p.barcode == result.contents }
+            if(product != null){
+                products.add(product)
+                productsRecyclerView.adapter?.notifyDataSetChanged()
+                Toast.makeText(this,"Escaneado: ${product.name}",Toast.LENGTH_LONG).show()
+            }
+            else{
+                Toast.makeText(this,"Producto no encontrado",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
